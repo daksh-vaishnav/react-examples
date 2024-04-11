aa06083d7ada7ae1ca67a910c677a32d40cc0366
daksh <vaishnav.dhaval.97@gmail.com>
2024-04-11 09:39:51 +0530

    Utils: restructure code

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
 import { useState } from "react";
 import "./App.css";
-import AddTodo from "./dredux/components/addTodo";
-import Todos from "./dredux/components/todos";
+import ReduxDemo from "./examples/dredux/reduxDemo";
 
 function App() {
-    //! redux toolkit example state Start
-    const [isEditTodo,setEditTodo] = useState(false)
-    const [editTodoDetail,setEditTodoDetail] = useState(false)
-    //? redux toolkit example state end
-
-
-
 	return (
-        //! redux toolkit example start
-		// <>
-        // {/* {`${isEditTodo} from app` } */}
-		// 	<AddTodo isEditTodo={isEditTodo} setEditTodo={setEditTodo} editTodoDetail={editTodoDetail}/>
-		// 	<Todos setEditTodo={setEditTodo} setEditTodoDetail={setEditTodoDetail}/>
-		// </>
-        //? redux toolkit example end
-
-
-
-        //! composition example start
         <>
-
+            <ReduxDemo />
         </>
 	);
 }
